f11948c8ce7cfa19107de7ddc67396b4056ea8ac
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-09-06 16:50:29 -0500

    Update Hola.js

--- plugins/Hola.js
+++ plugins/Hola.js
@@ -1,4 +1,4 @@
-let handler = m => m.reply('*Hola👋 soy 𝑩𝒂𝒕𝒓𝒐𝒍 𝒃𝒐𝒕 😊 Asistente de Axel🌀??* , *En que te puedo ayudar¿?*_Escribe_ *#help* o *#Menú* _para que puedas ver mis funciones_ .*')
+let handler = m => m.reply('*Hola👋 soy 𝑵𝒂𝒏𝒊 𝒃𝒐𝒕 😊 Asistente de Axel🌀??* , *En que te puedo ayudar¿?*_Escribe_ *#help* o *#Menú* _para que puedas ver mis funciones_ .*')
 
 handler.customPrefix = /^(Hola)$/i
 handler.command = new RegExp
