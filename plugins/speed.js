74f672fcf60d7ea58ef4909d1b1064009d6bea34
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-09-07 19:18:01 -0500

    Update speed.js

--- plugins/speed.js
+++ plugins/speed.js
@@ -1,78 +1,13 @@
-let os = require('os')
-let util = require('util')
 let { performance } = require('perf_hooks')
-let { sizeFormatter } = require('human-readable')
-let format = sizeFormatter({
-  std: 'JEDEC', // 'SI' (default) | 'IEC' | 'JEDEC'
-  decimalPlaces: 2,
-  keepTrailingZeroes: false,
-  render: (literal, symbol) => `${literal} ${symbol}B`,
-})
 let handler = async (m, { conn }) => {
-  const chats = conn.chats.all()
-  const groups = chats.filter(v => v.jid.endsWith('g.us'))
-  const groupsIn = groups.filter(v => !v.read_only)
-  const used = process.memoryUsage()
-  const cpus = os.cpus().map(cpu => {
-    cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
-    return cpu
-  })
-  const cpu = cpus.reduce((last, cpu, _, { length }) => {
-    last.total += cpu.total
-    last.speed += cpu.speed / length
-    last.times.user += cpu.times.user
-    last.times.nice += cpu.times.nice
-    last.times.sys += cpu.times.sys
-    last.times.idle += cpu.times.idle
-    last.times.irq += cpu.times.irq
-    return last
-  }, {
-    speed: 0,
-    total: 0,
-    times: {
-      user: 0,
-      nice: 0,
-      sys: 0,
-      idle: 0,
-      irq: 0
-    }
-  })
   let old = performance.now()
-  await m.reply('_⏱️ velocidad de prueba..._')
+  await m.reply('_Test de velocidad..._')
   let neww = performance.now()
-  let speed = neww - old
-  let txt = `
-Responder en ${speed} milisegundos
-
-🖥️ *_Estado_* 🖥️ :
-- Chats grupales *${groups.length}* 
-- Grupos unidos *${groupsIn.length}* 
-- Grupos a la izquierda *${groups.length - groupsIn.length}* 
-- Chats privados *${chats.length - groups.length}* 
-- Chats totales *${chats.length}* 
-
-🖥️ *_información del sistema_* 🖥️:
-${'```' + `
-🔋 Batería : ${conn.battery ? `${conn.battery.value}% ${conn.battery.live ? '🔌 Cargando...' : '⚡ Discagado'}` : 'Unknown'}
-${util.format(conn.user.phone)}
-`.trim() + '```'}
-
-📡 *_Servidor de información_* 📡:
-RAM: ${format(os.totalmem() - os.freemem())} / ${format(os.totalmem())}
-
-📟 *_Uso de memoria de Nodejs_* 📟:
-${'```' + Object.keys(used).map((key, _, arr) => `${key.padEnd(Math.max(...arr.map(v=>v.length)),' ')}: ${format(used[key])}`).join('\n') + '```'}
-
-${cpus[0] ? `📉 *_Uso total de la CPU_* 📉:
-${cpus[0].model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}
-
-_CPU Core(s) Usage (${cpus.length} Core CPU)_
-${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}`).join('\n\n')}` : ''}
-`.trim()
-  m.reply(txt)
+  m.reply(neww - old + 'ms')
+  
 }
 handler.help = ['ping', 'speed']
 handler.tags = ['info', 'tools']
 
-handler.command = /^(ping|speed|info)$/i
+handler.command = /^(ping|speed)$/i
 module.exports = handler
