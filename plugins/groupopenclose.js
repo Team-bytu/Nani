bb7f3055acd59ff070eedc3911a2ce45168b3cbe
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-09-07 19:02:45 -0500

    Update and rename Grup-seting.js to groupopenclose.js

--- plugins/groupopenclose.js
+++ plugins/groupopenclose.js
@@ -1,22 +1,25 @@
 let { Presence, GroupSettingChange } = require('@adiwajshing/baileys')
 let handler  = async (m, { conn, args, usedPrefix, command }) => {
 	let isClose = { // Switch Case Like :v
-		'open': false,
-		'close': true,
+		'abierto': false,
+		'cerrado': true,
 	}[(args[0] || '')]
 	await conn.updatePresence(m.chat, Presence.composing)
 	if (isClose === undefined)
 		throw `
-*ғᴏʀᴍᴀᴛ ᴡʀᴏɴɢ! ᴇxᴀᴍᴘʟᴇ :*
-
-  *○ ${usedPrefix + command} off*
-  *○ ${usedPrefix + command} on*
+*_¡Formato erróneo!_*
+  
+   ──────❲ *🌀Ejemplo🌀:* ❳────────
+   
+  *├┉↯ ${usedPrefix + command} cerrado*
+  *├┉↯ ${usedPrefix + command} abierto*
 `.trim()
 	await conn.groupSettingChange(m.chat, GroupSettingChange.messageSend, isClose)
+{m.reply('*Se realizó con exito✅*')}
 }
-handler.help = ['group *on / off*']
+handler.help = ['grupo *abierto / cerrado*']
 handler.tags = ['group']
-handler.command = /^(group)$/i
+handler.command = /^(grupo)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
