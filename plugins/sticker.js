ab69ef75250aa06aaf0a0883ab97c3c59c7fd28b
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-09-07 19:26:00 -0500

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -1,27 +1,23 @@
 const { MessageType } = require('@adiwajshing/baileys')
 const { sticker } = require('../lib/sticker')
 let handler  = async (m, { conn, args }) => {
-  let stiker = false 
-  
- 
-    esperar  m . responder ( '🌀 * Tu stiker está en progreso ... * 🌀' )
-    }
+  let stiker = false
   try {
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || ''
     if (/image|video/.test(mime)) {
       let img = await q.download()
-      if (!img) throw 'Foto/Video extraviado'
-      stiker = await sticker(img, true, global.packname, global.author)
-    } else if (args[0]) stiker = await sticker(true, args[0], global.packname, global.author)
+      if (!img) throw 'Por favor cargue o etiquete una foto o video/gif'
+      stiker = await sticker(img, false, global.packname, global.author)
+    } else if (args[0]) stiker = await sticker(false, args[0], global.packname, global.author)
   } finally {
     if (stiker) conn.sendMessage(m.chat, stiker, MessageType.sticker, {
       quoted: m
     })
-    else throw 'Etiqueta una foto o vídeo hasta 10 seg'
+    else throw 'Conversion failed'
   }
 }
-handler.help = ['stciker (caption|reply media)', 'stciker <url>', 'stickergif (caption|reply media)', 'stickergif <url>']
+handler.help = ['sticker']
 handler.tags = ['sticker']
 handler.command = /^s(tic?ker)?(gif)?(wm)?$/i
 
